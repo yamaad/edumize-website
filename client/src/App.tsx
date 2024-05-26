00b2d1c18dd9834a46cb28362926982e8475c9b9
yamaad <maad@quickdesk.io>
2024-05-26 22:18:14 +0800

    debug imports issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,18 @@
-// import { ComponentRoute, PageRoute } from "./client/src/routes/constant.ts";
-// import { ProgramProfile } from "./client/src/features/Program/ProgramProfile.feature";
-// import { UniversityProfile } from "./features/UniversityProfile/UniversityProfile.feature.tsx";
+import { ComponentRoute, PageRoute } from "./routes/constant.ts";
+import { ProgramProfile } from "./features/Program/ProgramProfile.feature.tsx";
+import { UniversityProfile } from "./features/UniversityProfile/UniversityProfile.feature.tsx";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>HOME PAGE 1</h1>} />
-      {/* <Route path={PageRoute.university}> */}
-      {/* <Route path={ComponentRoute.universityCourses} element={<UniversityProfile universityId={213} />} /> */}
-      {/* </Route> */}
-      {/* <Route path={PageRoute.program}> */}
-      {/* <Route path={ComponentRoute.programCourses} element={<ProgramProfile programId={"E05APP0XXX"} />} /> */}
-      {/* </Route> */}
+      <Route path={PageRoute.university}>
+        <Route path={ComponentRoute.universityCourses} element={<UniversityProfile universityId={213} />} />
+      </Route>
+      <Route path={PageRoute.program}>
+        <Route path={ComponentRoute.programCourses} element={<ProgramProfile programId={"E05APP0XXX"} />} />
+      </Route>
       <Route path="*" element={<>404 Page not found</>} />
     </Routes>
   );
