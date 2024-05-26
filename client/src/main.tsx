57410823703ea5b82d807c63bf13a7fd0fc7e243
yamaad <maad@quickdesk.io>
2024-05-26 22:47:11 +0800

    debug imports issue

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.ts";
-import { ProgramProfile } from "./features/Program/ProgramProfile.feature.tsx";
-import { UniversityProfile } from "./features/UniversityProfile/UniversityProfile.feature.tsx";
+import { store } from "redux/store";
+import { ProgramProfile } from "features/Program/ProgramProfile.feature";
+import { UniversityProfile } from "features/UniversityProfile/UniversityProfile.feature";
 
 
 // For WebFlow Embedding
