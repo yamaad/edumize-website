2f2e62d595e77c17a0bb333980220ca0703e742e
yamaad <maad@quickdesk.io>
2024-06-09 03:35:32 +0800

    env variables not read

--- client/src/redux/currency/currency.api.ts
+++ client/src/redux/currency/currency.api.ts
@@ -1,4 +1,3 @@
-// Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Currencies, Rates } from "./currency.model";
 // Define a service using a base URL and expected endpoints
